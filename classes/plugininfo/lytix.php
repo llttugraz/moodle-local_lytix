44975aac2211abf88d09de5bdd51e7470fe85ad3
Kemal Halilovic <kemal.halilovic@tugraz.at>
2024-08-21 17:10:16 +0200

    [#155] codechecker

--- classes/plugininfo/lytix.php
+++ classes/plugininfo/lytix.php
@@ -14,6 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * Helper.
+ *
+ * @package   local_lytix
+ * @copyright 2024 Educational Technologies, Graz, University of Technology
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 namespace local_lytix\plugininfo;
 /**
  * Class helper
